1968d1d5d15714acae4878b4e6c93c41754e06bb
Ulf Krantz <u@nulf.se>
2020-10-03 03:34:08 +0200

    Rename input parameters accoring to new comman structure

--- sermon.sh
+++ sermon.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-server=$1
-specific_test=$2
+# Sermon connects to a list of servers using the current users ssh-keys and relays back the current server load
+
+# Fist parameter is the command to run
+command=$1
+
+# Second parameter is the data to input to the command
+parameter=$2
+
+
+
